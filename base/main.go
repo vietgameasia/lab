e8ddf73fa4a2a40b1b13f2812b842fa87939b769
Nguyen Do <khoanguyen.do@outlook.com>
2025-03-07 09:46:36 +0700

    feat: initial commit

---
+++ base/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+
+		return se.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
